90fd4dfe48e82c4bc55f46676a4e216bda0db7e8
antebotic <ante.botic_ext@autoscout24.com>
2021-06-18 06:30:09 +0200

    Solidity Course - Lottery Contract Challenges 1-4

--- SolidityFromScratch/Section4-LotterySmartContract/Lottery.sol
+++ SolidityFromScratch/Section4-LotterySmartContract/Lottery.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.4;
 
 /*
-  - lottery starts bu accepting ETH transactions. Anyone with an ETH waller can send a 
+  - lottery starts by accepting ETH transactions. Anyone with an ETH wallet can send a 
     fixed amount of 0.1 ETH to the contract address
   - the sender address is registered, contract allows multiple sends
   - if there are at lease three players, at a point in time, random winner is picked,
@@ -12,19 +12,29 @@ pragma solidity ^0.8.4;
 
   - deployed and tested on rinkeby here:
       https://rinkeby.etherscan.io/address/0x0276ad5996bacd8332cf7f80be8debfdb3b127ec
+
+  Challenge #1
+    - Change the contract so that the manager of the lottery can not participate in the lottery.
+  Challenge #2
+    - Change the contract so that the manager is automatically added to the lottery, without sending any ether.
+  Challenge #3
+    Change the contract so that anyone can pick the winner and finish the lottery, if there are at least 10 players.
+  Challenge #4
+    Change the contract so that the manager receives a fee of 10% of the lottery funds.
  */
 
 
 contract Lottery {
   address payable[] public players;
-  address public manager;
+  address payable public  manager;
 
   constructor() {
-    manager = msg.sender;
+    manager = payable(msg.sender);
   }
-
+  
   //only one receive address per contract, introduced in 0.6.0
   receive() external payable{
+    require(msg.sender != manager, "manager can't participate"); // Challenge #1
     require(msg.value == 0.1 ether, "amount sent was not 0.1ETH");
     players.push(payable(msg.sender)); // it is necessary to convert regular address to a payable one due to players being payable array
   }
@@ -56,5 +66,23 @@ contract Lottery {
 
     //reset lottery
     players = new address payable[](0); // new empty dynamic array of type address
+  }
+
+  function pickWinnerChallenge3And4() public {
+    require(players.length >= 10, "not enough players to end lottery");
+    
+    uint r = random();
+    address payable winner;
+
+    uint index = r % players.length;
+    uint inShare = getBalance() / 10;
+    
+    winner = players[index];
+    players.push(manager);
+
+    winner.transfer(inShare * 9);
+    manager.transfer(inShare);
+
+    players = new address payable[](0);
   }
 }
