2ddbea79f7c8fbdef1fda22417c87fdb37da37cc
antebotic <ante.botic_ext@autoscout24.com>
2021-07-26 08:32:08 +0200

    OpenZeppelin - Start Learn docs, Truffle setup

---
+++ Security/OpenZeppelin/Docs/Truffle/contracts/Box.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+  uint private _value;
+
+  event ValueChanged(uint value);
+
+  function store(uint value) public onlyOwner {
+    _value = value;
+    emit ValueChanged(_value);
+  }
+
+  function retrieve() public view returns (uint) {
+    return _value;
+  }
+
+}
+
+
+import "./access-control/Auth.sol";
+
+contract OldBox {
+  uint private _value;
+  Auth private _auth;
+
+  constructor(Auth auth) {
+    _auth = auth;
+  }
+
+  event ValueChanged(uint value);
+
+  function store(uint value) public {
+    require(_auth.isAdministrator(msg.sender), "Unauthorized");
+
+    _value = value;
+    emit ValueChanged(_value);
+  }
+
+  function retrieve() public view returns (uint) {
+    return _value;
+  }
+}
